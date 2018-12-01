9bd6be7a31f886e6abe7cf6a815eff20f7051f24
Matthew Horwell <mattjhorwell@gmail.com>
2018-12-01 20:14:47 +0000

    Now saves tiles that have been stepped on before so description will be the same upon returning to a tile.

--- src/theInfiniteTerrainSimulator/Game.java
+++ src/theInfiniteTerrainSimulator/Game.java
@@ -7,61 +7,51 @@ import java.util.Scanner;
 public class Game {
 
 	public void playGame() {
-		String cont = "y"; 
+		String cont = "y";
 		Player pete = new Player(0, 0);
 		Feature newFeature = new Feature(1, 1, 1);
-		Tile startTile = new Tile(pete.getPlayerXCoordinate(),
-				pete.getPlayerYCoordinate());
+		Tile startTile = new Tile(pete.getPlayerXCoordinate(), pete.getPlayerYCoordinate());
 		startTile.setTileType(startTile.generateTileType());
 		List<Tile> tileList = new LinkedList<Tile>();
 		tileList.add(startTile);
 		System.out.println(startTile.getTileDescription(startTile.getTileType()));
-		
-		
-		
+
 		while (cont.equals("y")) {
-			
+
 			Scanner userInput = new Scanner(System.in);
 			String direction = userInput.nextLine();
 			pete.movePlayer(direction);
+			boolean tileExists = false;
 
-			
 			for (Tile tiles : tileList) {
-				System.out.print(tiles.getTileType());
-				if (pete.getPlayerXCoordinate() == tiles.getTileX() && pete.getPlayerYCoordinate() == tiles.getTileY()){
-					System.out.println("It's a match");
+				if (pete.getPlayerXCoordinate() == tiles.getTileX()
+						&& pete.getPlayerYCoordinate() == tiles.getTileY()) {
+					tileExists = true;
 					System.out.println(tiles.getTileDescription(tiles.getTileType()));
-					System.out.print(tiles.getTileType());
-				}
-				else {
-					Tile newTile = new Tile(pete.getPlayerXCoordinate(),
-							pete.getPlayerYCoordinate());
-					newTile.setTileType(newTile.generateTileType());
-					tileList.add(newTile);
-					System.out.println("New tile");
-					System.out.println(newTile.getTileDescription(newTile.getTileType()));
+					System.out.println("Nearest feature is " + pete.calculateDistance(newFeature, pete) + "m away.");
 					break;
 				}
-				
 			}
-			System.out.println("Nearest feature is " + pete.calculateDistance(newFeature, pete) + "m away.");
-			
-			if (pete.getPlayerXCoordinate() == newFeature.getFeatureXCoord() &&
-				pete.getPlayerYCoordinate() == newFeature.getFeatureYCoord()) {
+
+			if (tileExists == false) {
+				Tile newTile = new Tile(pete.getPlayerXCoordinate(), pete.getPlayerYCoordinate());
+				newTile.setTileType(newTile.generateTileType());
+				System.out.println(newTile.getTileDescription(newTile.getTileType()));
+				tileList.add(newTile);
+				System.out.println("Nearest feature is " + pete.calculateDistance(newFeature, pete) + "m away.");
+			}
+
+
+			if (pete.getPlayerXCoordinate() == newFeature.getFeatureXCoord()
+					&& pete.getPlayerYCoordinate() == newFeature.getFeatureYCoord()) {
 				System.out.println(newFeature.featureDescription(newFeature.getFeatureNumber()));
 				System.out.println("Continue? (y or n)");
 				cont = userInput.nextLine();
-				newFeature = new Feature(newFeature.generateTileType(),
-						newFeature.setFeatureXCoord(),
-						newFeature.setFeatureYCoord(),
-						newFeature.setFeatureNumber());
+				newFeature = new Feature(newFeature.generateTileType(), newFeature.setFeatureXCoord(),
+						newFeature.setFeatureYCoord(), newFeature.setFeatureNumber());
 			}
 
 		}
 	}
-	
-
-
-	
 
 }
